75b79af4ac212c933bf0eef0494a8085c9e98c7f
Sonya <sonya.karpovich@gmail.com>
2019-05-01 13:55:11 +0800

    Поправки после проверки

--- gulpfile.js
+++ gulpfile.js
@@ -52,12 +52,12 @@ gulp.task("images", function () {
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
     ]))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 gulp.task("webp", function () {
   return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("source/img"));
+    .pipe(gulp.dest("build/img"));
 });
 gulp.task("clean", function () {
   return del("build");
@@ -90,5 +90,5 @@ gulp.task("refresh", function (done) {
   done();
 });
 
-gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html"));
+gulp.task("build", gulp.series("clean", "copy", "css", "sprite", "html", "images", "webp"));
 gulp.task("start", gulp.series("build", "server"));
