e937d07996af635c1e25786aa8597fb4bb6bf14d
Sonya <sonya.karpovich@gmail.com>
2019-05-11 17:37:02 +0800

    Доверстала слайдер

--- source/js/cat-example.js
+++ source/js/cat-example.js
@@ -3,15 +3,22 @@ var beforeImage = document.querySelector('.example__image--before');
 var afterImage = document.querySelector('.example__image--after');
 var beforeToggle = document.querySelector('.example__toggle-label--before');
 var afterToggle = document.querySelector('.example__toggle-label--after');
+var imageWrapper = document.querySelector('.example__column-wrapper--cat');
+var control = document.querySelector('.example__control');
+var bar = document.querySelector('.example__bar');
 
 beforeToggle.addEventListener('click', function() {
-  example.classList.remove('example--after');
-  beforeImage.classList.remove('example__image--hidden');
-  afterImage.classList.add('example__image--hidden');
+  beforeImage.style.width = '100%';
+  afterImage.style.width = '0%';
+  imageWrapper.style.background = '#f2f2f2';
+  control.classList.add('example__control--before');
+  bar.classList.add('example__bar--before');
 });
 
 afterToggle.addEventListener('click', function() {
-  example.classList.add('example--after');
-  afterImage.classList.remove('example__image--hidden');
-  beforeImage.classList.add('example__image--hidden');
+  afterImage.style.width = '100%';
+  beforeImage.style.width = '0%';
+  imageWrapper.style.background = '#eaeaea';
+  control.classList.add('example__control--after');
+  bar.classList.add('example__bar--after');
 });
